f46599999e4fbf34df6a0dd832bd40a4f233405c
Jttanner <jonttanner@gmail.com>
2017-09-29 15:58:34 -0600

    tests

---
+++ server/src/test/java/ServerModel/ServerFacadeTest.java
@@ -0,0 +1,59 @@
+package ServerModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import modeling.Game;
+import modeling.User;
+import modeling.UserInfo;
+import request.LoginRequest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jontt on 9/29/2017.
+ */
+public class ServerFacadeTest {
+
+    Map<String, Game> games = new HashMap<>();
+    Map<String, User> users = new HashMap<>();
+
+    ServerFacade facade = new ServerFacade();
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+        users.put("user", new User(new UserInfo("user", "password", "test")));
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void login() throws Exception {
+        //assertTrue(facade.login(new LoginRequest((new UserInfo("user", "password", "test")))));
+    }
+
+    @org.junit.Test
+    public void register() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void createGame() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void joinGame() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getGameList() throws Exception {
+
+    }
+
+}
